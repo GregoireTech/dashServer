25359074734f01401783aedbe53ce1e3fe0e1e75
Gregoire888 <greg.claise@gmail.com>
2019-05-19 23:54:49 +0200

    with env variables fully configured

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-module.exports = {
-    googleClientID: '416812458984-ra99mkq4jhe0m94n3s0un73dqo8ba9ds.apps.googleusercontent.com',
-    googleClientSecret: 'uWUm9zaY-Nfrxus8xl1stg9h',
-    dbUser: 'b771c9ee0d15eb',
-    dbName: 'heroku_a9227eda5f9398f',
-    dbPass: '753e08c9'
+
+
+if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
 }
