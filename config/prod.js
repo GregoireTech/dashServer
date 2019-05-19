c578aa4cdd68be6b707ad1bb8f860e7b3610749e
Gregoire888 <greg.claise@gmail.com>
2019-05-19 23:55:59 +0200

    with env variables fully configured (finally)

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    dbUser: process.env.DB_USER,
+    dbName: process.env.DB_NAME,
+    dbPass: process.env.DB_PASS
+}
