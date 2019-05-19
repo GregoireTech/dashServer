5c878da56728d8b3cb95d0b9c86813e1fe41b72b
Gregoire888 <greg.claise@gmail.com>
2019-05-19 23:35:03 +0200

    with env variables

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
 module.exports = {
     googleClientID: '416812458984-ra99mkq4jhe0m94n3s0un73dqo8ba9ds.apps.googleusercontent.com',
-    googleClientSecret: 'uWUm9zaY-Nfrxus8xl1stg9h'
+    googleClientSecret: 'uWUm9zaY-Nfrxus8xl1stg9h',
+    dbUser: 'b771c9ee0d15eb',
+    dbName: 'heroku_a9227eda5f9398f',
+    dbPass: '753e08c9'
 }
--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
+const keys = require('../config/keys');
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'us-cdbr-iron-east-01.cleardb.net',
-    user: 'b771c9ee0d15eb',
-    database:'heroku_a9227eda5f9398f',
-    password: '753e08c9'
+    user: keys.dbUser,
+    database: keys.dbName,
+    password: dbPass
 });
 
 module.exports = pool.promise();
